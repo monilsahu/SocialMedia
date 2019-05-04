084e64c8143fa312d0fa223af5a35542303524fb
Monil Sahu <33417952+monilsahu@users.noreply.github.com>
2019-05-04 20:19:14 +0530

    Add files via upload

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBLiYBk7f-WjurS6WJEn4_kRLFw8J04XEc",
+    authDomain: "appp-34d83.firebaseapp.com",
+    databaseURL: "https://appp-34d83.firebaseio.com",
+    projectId: "appp-34d83",
+    storageBucket: "",
+    messagingSenderId: "328344497545"
+  };
+
+export default config
